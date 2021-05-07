17cf47cb75533650ce1565a0a5e1ea47000a95b4
William Boswall <wboswall@live.com>
2021-05-07 12:47:58 -0300

    Update and rename engine.js to play.js
    Renamed to play for sandbox webpage.  Don't want the engine.js to be mucked up and hard to maintain.

--- js/play.js
+++ js/play.js
@@ -53,3 +53,78 @@ var pi, person, answer; pi=3.14; person="John Doe"; answer='Yes I am!';
 var carName; carName="Volvo"; document.getElementByID("demo".innerHTML =carName;
 var x=5+2=3;
 var $myString; $myString="String";
+// code here can NOT use carName
+function myFunction() {
+  var carName = "Volvo";
+  // code here CAN use carName
+}
+
+// code here can NOT use carName 
+{
+  var x = 2;
+}
+// x CAN be used here 
+{
+  let x = 2;
+}
+// x can NOT be used here 
+ var x = 10;
+// Here x is 10
+{
+  var x = 2;
+  // Here x is 2
+}
+// Here x is 2 
+ var x = 10;
+// Here x is 10
+{
+  let x = 2;
+  // Here x is 2
+}
+// Here x is 10 
+ var i = 5;
+for (var i = 0; i < 10; i++) {
+  // some statements
+}
+// Here i is 10 
+ let i = 5;
+for (let i = 0; i < 10; i++) {
+  // some statements
+}
+// Here i is 5 
+function myFunction() {
+  var carName = "Volvo";   // Function Scope
+}
+function myFunction() {
+  let carName = "Volvo";   // Function Scope
+}
+var x = 2;       // Global scope
+let x = 2;       // Global scope
+ var carName = "Volvo";
+// code here can use window.carName 
+ let carName = "Volvo";
+// code here cannot use window.carName 
+ var x = 2;
+// Now x is 2
+ var x = 3;
+// Now x is 3 
+var x = 2;       // Allowed
+let x = 3;       // Not allowed
+
+{
+  var x = 4;   // Allowed
+  let x = 5   // Not allowed
+}
+carName = "Volvo";
+alert(carName);
+var carName; // is called hoisting
+ const PI = 3.141592653589793;
+PI = 3.14;      // This will give an error
+PI = PI + 10;   // This will also give an error 
+var x = 10;
+// Here x is 10
+{
+  const x = 2;
+  // Here x is 2
+}
+// Here x is 10 
