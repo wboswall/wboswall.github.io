3e8b7e4a9a47f598e66354bcd4cce442ed51d01a
William Boswall <wboswall@live.com>
2021-05-07 12:22:43 -0300

    Update engine.js
    Added more content.

--- js/engine.js
+++ js/engine.js
@@ -1,5 +1,53 @@
 //Single line comment
-/**
-  * Multiline
-  * comment
-/**
+/*
+multiline comment
+*/
+/*
+  * Formal comment
+  * for official documentation
+*/
+document.getElementById("demo").innerHTML = 5 + 6;
+//document.write(5 + 6);
+//window.alert(5 + 6);
+console.log(5 + 6);
+window.print()
+var x,y,z;
+x=5;
+y=6;
+z=x+y;
+document.getElementbyId("demo").innerHTML="Hello Dolly."
+var a,b,c; a=5; b=6; c=a+b;
+var person="Hege";
+var x = y + z;
+document.getElementById("demo").innerHTML = 
+  "Hello Dolly!";
+function myFunction() {
+    document.getElementnById("demo1").innerHTML = "Hello Dolly!";
+    document.getElementById("demo2").innerHTML = "How are you?";
+}
+var string1, string2
+string1="John Doe"; string2='John Doe';
+var num1, num2;
+num1=10.50; num2=1001;
+//(5+6)*10
+//5/6
+//5-1
+var lastname, lastName;
+lastName="Doe";
+lastname="Peterson";
+/*
+ * The code below will change
+ * the heading with id ="myH"
+ * and the paragraph with id ="myP"
+ * in my web page:
+ */
+document.getElementById("myH").innerHTML ="My First Page";
+document.getElementById("myP").innerHTML = "My first paragraph.";
+//use let for variables inside functions and var for global variables
+let x; x=1;
+const =NUM1; NUM1=1;
+var price1, price2, total; price=5; price2=6; total=price1 +price2;
+x--;
+x++;
+var pi, person, answer; pi=3.14; person="John Doe"; answer='Yes I am!';
+var carName; carName="Volvo"; document.getElementByID("demo".innerHTML =carName;
